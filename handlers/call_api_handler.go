0fa8d2db18c109164185b52ff7b5bffbab811768
Corey <xenios91@protonmail.com>
2022-01-02 04:06:25 +0000

    added more header stuff

--- handlers/call_api_handler.go
+++ handlers/call_api_handler.go
@@ -1,16 +1,59 @@
 package Handlers
 
 import (
+	"errors"
 	"fmt"
 	"html/template"
 	"io"
 	"log"
 	"net/http"
-	"strings"
+	"regexp"
 )
 
 func checkURL(url string) bool {
-	return strings.HasPrefix(url, "https://") || strings.HasPrefix(url, "http://")
+	regex := `https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)`
+
+	match, err := regexp.MatchString(regex, url)
+
+	return match && (err == nil)
+}
+
+func submitHTTPRequest(httpMethod, url, contentType string, body io.Reader) (*string, error) {
+	var b []byte
+	var err error
+	var resp *http.Response
+
+	switch httpMethod {
+	case "GET":
+		if resp, err = http.Get(url); err != nil {
+			log.Println("An error has occured")
+		} else {
+			b, err = io.ReadAll(resp.Body)
+			if err != nil {
+				log.Fatalln(err)
+			}
+
+			resp := string(b)
+			return &resp, err
+		}
+
+	case "POST":
+		if resp, err = http.Post(url, contentType, body); err != nil {
+			log.Println("An error has occured")
+		} else {
+			b, err = io.ReadAll(resp.Body)
+			if err != nil {
+				log.Fatalln(err)
+			}
+
+			resp := string(b)
+			return &resp, err
+		}
+
+	default:
+		break
+	}
+	return nil, errors.New("unknown error")
 }
 
 func getAPIResponse(w http.ResponseWriter, r *http.Request) {
@@ -28,41 +71,18 @@ func getAPIResponse(w http.ResponseWriter, r *http.Request) {
 		}
 		var httpMethod string = r.FormValue("http-method")
 		var url string = r.FormValue("url")
-		var contentType string
+		var contentType string = r.FormValue("content-type")
 		var body io.Reader
 
 		if !checkURL(url) {
-			url = fmt.Sprintf("http://%s", url)
+			http.Error(w, "Invalid URL", http.StatusBadRequest)
+			return
 		}
 
-		switch httpMethod {
-		case "GET":
-			if resp, err := http.Get(url); err != nil {
-				log.Println("An error has occured")
-				http.Error(w, "Server Error", http.StatusInternalServerError)
-			} else {
-				b, err := io.ReadAll(resp.Body)
-				if err != nil {
-					log.Fatalln(err)
-				}
-
-				resp := string(b)
-				fmt.Fprint(w, resp)
-			}
-
-		case "POST":
-			if resp, err := http.Post(url, contentType, body); err != nil {
-				log.Println("An error has occured")
-				http.Error(w, "Server Error", http.StatusInternalServerError)
-			} else {
-				b, err := io.ReadAll(resp.Body)
-				if err != nil {
-					log.Fatalln(err)
-				}
-
-				resp := string(b)
-				fmt.Fprint(w, resp)
-			}
+		if resp, err := submitHTTPRequest(httpMethod, url, contentType, body); err != nil {
+			http.Error(w, "An error has occured", http.StatusBadRequest)
+		} else {
+			fmt.Fprint(w, *resp)
 		}
 	}
 }
