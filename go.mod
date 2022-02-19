8acaa5860454b52848025e8abbba79d22383c354
Corey <xenios91@protonmail.com>
2022-02-19 01:57:56 +0000

    removed graylog for now until a need comes.

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module EchoBot
 
 go 1.17
-
-require gopkg.in/Graylog2/go-gelf.v2 v2.0.0-20191017102106-1550ee647df0
--- main.go
+++ main.go
@@ -5,12 +5,8 @@ import (
 	service "EchoBot/service"
 	"flag"
 	"fmt"
-	"io"
 	"log"
 	"net/http"
-	"os"
-
-	"gopkg.in/Graylog2/go-gelf.v2/gelf"
 )
 
 func main() {
@@ -26,25 +22,11 @@ func startServices() {
 	service.RunTimedTask()
 }
 
-func grayLogConfig(graylogAddr *string) {
-	if len(*graylogAddr) > 0 {
-		gelfWriter, err := gelf.NewTCPWriter(*graylogAddr)
-		if err != nil {
-			log.Fatalf("gelf.NewWriter: %s", err)
-		}
-		// log to both stderr and graylog2
-		log.SetOutput(io.MultiWriter(os.Stderr, gelfWriter))
-		log.Printf("logging to stderr & graylog2 at: [%s]\n", *graylogAddr)
-	}
-}
-
 func startServer() {
 	loadHandlers()
 
-	var graylogServer = flag.String("graylog", "", "The graylog server and port to utilize [Example: -graylog=http:www.graylog.com:8080")
 	var port = flag.Int("port", 8080, "Port number for EchoBot to run on. [Example: -port=8888")
 
-	grayLogConfig(graylogServer)
 	startServices()
 
 	log.Printf("EchoBot server started on port %d!\n", port)
